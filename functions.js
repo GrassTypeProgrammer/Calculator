639330643de56c4c68f8da0fa1159bba42a0cca7
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2024-01-23 08:57:00 +0000

    Initial commit. Most of html and styles setup.

---
+++ functions.js
@@ -0,0 +1,11 @@
+
+// function initialise(){
+//     const NumberPad = document.getElementById('NumberPad');
+
+//     for (let i = 0; i < 4; i++) {
+//         const row = document.createElement('div');
+//         row.classList.add('Calculator_row');
+//         const number        
+//     }
+
+// }
