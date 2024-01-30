7e495633ece4738d6afe13fbd9456ba23a870be3
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2024-01-30 08:18:38 +0000

    The calculations are now working correctly.

--- functions.js
+++ functions.js
@@ -1,11 +1,75 @@
 
-// function initialise(){
-//     const NumberPad = document.getElementById('NumberPad');
+_currentSymbols = [];
+_currentNumbers = [];
+_symbols = ['+', '-', '*', '/'];
 
-//     for (let i = 0; i < 4; i++) {
-//         const row = document.createElement('div');
-//         row.classList.add('Calculator_row');
-//         const number        
-//     }
+function addSymbol(symbol){
+    if(_symbols.includes(symbol)){
+        if(_currentSymbols.length == _currentNumbers.length){
+            _currentSymbols[_currentSymbols.length-1] = symbol;
+        }
+        else{
+            _currentSymbols.push(symbol);
+        }
+    }
 
-// }
+    printArray(_currentSymbols);
+}
+
+function addNumber(num){
+    if(_currentSymbols.length == _currentNumbers.length){
+        _currentNumbers.push(num + '');
+    }
+    else{
+        _currentNumbers[_currentNumbers.length - 1] += (num + '');
+    }
+
+    printArray(_currentNumbers);
+}
+
+function solveEquation(){
+        for (let index = 0; index < _currentSymbols.length; index++) {
+            const symbol = _currentSymbols[index];
+            let ans;
+
+            if(symbol == '*' || symbol == '/'){
+                if(symbol == '*' ){
+                    ans = parseFloat(_currentNumbers[index]) * parseFloat(_currentNumbers[index + 1]);
+                }
+                else if(symbol == '/'){
+                    ans = parseFloat(_currentNumbers[index] )/ parseFloat(_currentNumbers[index + 1]);
+                }
+                
+                _currentNumbers[index] = ans.toString();
+                _currentNumbers.splice(index + 1, 1);
+                _currentSymbols.splice(index, 1);
+                index--;
+            }
+        }
+
+        for (let index = 0; index < _currentSymbols.length; index++) {
+            const symbol = _currentSymbols[index];
+            let ans;
+
+            if(symbol == '+' || symbol == '-'){
+                if(symbol == '+' ){
+                    ans = parseFloat(_currentNumbers[index]) + parseFloat(_currentNumbers[index + 1]);
+                }
+                else if(symbol == '-'){
+                    ans = parseFloat(_currentNumbers[index]) - parseFloat(_currentNumbers[index + 1]);
+                }
+                
+                _currentNumbers[index] = ans.toString();
+                _currentNumbers.splice(index + 1, 1);
+                _currentSymbols.splice(index, 1);
+                index--;
+            }
+        }
+    }
+
+function printArray(array){
+    console.log('Array: ');
+    array.forEach(element => {
+        console.log(element);
+    });
+}
