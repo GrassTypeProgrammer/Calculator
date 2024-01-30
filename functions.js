e9aee3204623fd310bf1edc0909aef87abca9c66
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2024-01-30 08:54:48 +0000

    The screen will now display the typed equation and answer. Various screen utility functions have been added.

--- functions.js
+++ functions.js
@@ -2,30 +2,47 @@
 _currentSymbols = [];
 _currentNumbers = [];
 _symbols = ['+', '-', '*', '/'];
+_screenDisplay = '';
+_showingAnswer = false;
+
 
 function addSymbol(symbol){
+    if(_showingAnswer){
+        _showingAnswer = false;
+        clearAll();
+    }
+
     if(_symbols.includes(symbol)){
         if(_currentSymbols.length == _currentNumbers.length){
             _currentSymbols[_currentSymbols.length-1] = symbol;
+            replaceLastSymbolOnScreen(symbol);
         }
         else{
             _currentSymbols.push(symbol);
-        }
+            addNewValueToScreen(symbol, true);
+    }
     }
 
-    printArray(_currentSymbols);
 }
 
+
 function addNumber(num){
+    if(_showingAnswer){
+        _showingAnswer = false;
+        clearAll();
+    }
+
     if(_currentSymbols.length == _currentNumbers.length){
         _currentNumbers.push(num + '');
+        addNewValueToScreen(num);
+
     }
     else{
         _currentNumbers[_currentNumbers.length - 1] += (num + '');
+        addToExistingValueOnScreen(num);
     }
-
-    printArray(_currentNumbers);
 }
+
 
 function solveEquation(){
         for (let index = 0; index < _currentSymbols.length; index++) {
@@ -65,6 +82,11 @@ function solveEquation(){
                 index--;
             }
         }
+
+        ans = _currentNumbers[0];
+        clearAll();
+        addNewValueToScreen(ans);
+        _showingAnswer = true;
     }
 
 function printArray(array){
@@ -73,3 +95,53 @@ function printArray(array){
         console.log(element);
     });
 }
+
+
+function clearAll(){
+    _currentNumbers = [];
+    _currentSymbols = [];
+    clearScreen();
+}
+
+
+//#region Screen Functionality
+
+function addNewValueToScreen(value, isSymbol = false){
+    if(isSymbol){
+        _screenDisplay += ' ' + value + ' ';
+    }
+    else{
+        _screenDisplay += value + '';
+    }
+    updateScreenDisplay();
+}
+
+function addToExistingValueOnScreen(value){
+    _screenDisplay += value;
+    updateScreenDisplay();
+}
+
+function replaceLastSymbolOnScreen(value){
+    _screenDisplay.splice[_screenDisplay.length -2, 2];
+    _screenDisplay += value + ' ';
+    updateScreenDisplay();
+}
+
+function updateScreenDisplay(){
+    const screen = document.getElementById('screenDisplay');
+    screen.innerText = _screenDisplay;
+}
+
+function clearScreen(){
+    _screenDisplay = '';
+    updateScreenDisplay();
+
+}
+
+
+
+function backspace(){
+    // needs to remove from screen and current symbol/num
+}
+
+//#endregion
